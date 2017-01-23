c0ca0ec503770eb8360d9bf7b7902f19146fd58b
Chris Feijoo <christophe@feijoo.fr>
2017-01-23 01:47:31 +0100

    Rewrite getAccessorChain without using ES2015 Symbol

--- src/accessorChain.ts
+++ src/accessorChain.ts
@@ -8,34 +8,34 @@
      ## ## ## :##
       ## ## ##*/
 
-const ACCESSOR_CHAIN = Symbol()
-
 /**
- * Returns a proxy object that will fill accessor chain when accessing a member
+ * Return a Proxy that pushes accessors to accessorChain, recursively
  */
-let currentAccessorChain: string[] = []
-
-const ProxyAccessorChain: any = new Proxy({}, {
-  get: (node: any, name: string | symbol) => {
-    // Get accessors chain and clear it for next use
-    if (name === ACCESSOR_CHAIN) {
-      const accessorChain = currentAccessorChain
-      currentAccessorChain = []
-      return accessorChain
+const createAccessorChainFiller = (accessorChain: string[]) => {
+  const fillerProxy: ProxyHandler<any> = new Proxy(
+    {},
+    {
+      get: (node: any, name: string) => {
+        // Push current accessor to accessor chain
+        accessorChain.push(name)
+        // Fill accessors recursively
+        return fillerProxy
+      }
     }
-    else {
-      // Push current accessor to accessor chain
-      currentAccessorChain.push(name as string)
-      return ProxyAccessorChain
-    }
-  }
-})
+  )
+  return fillerProxy
+}
 
 /**
- * Returns accessors chain given an accessor expression
- * e.g. `_ => _.a.b` will return `['a', 'b']`
+ * Transform accessor expression to accessors chain
+ * e.g. (_ => _.a.b.c) will return ['a', 'b', 'c']
  */
 export const getAccessorChain = (accessor: (_: any) => any) => {
-  accessor(ProxyAccessorChain)
-  return ProxyAccessorChain[ACCESSOR_CHAIN]
+  const accessorChain: string[] = []
+  const accessorChainFiller = createAccessorChainFiller(accessorChain)
+
+  // Fill accessor chain
+  accessor(accessorChainFiller)
+
+  return accessorChain
 }
