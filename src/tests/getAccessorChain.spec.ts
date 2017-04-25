c3e76d57021b41313dd3d0a6253f5f74cf36c877
Chris Feijoo <christophe@feijoo.fr>
2017-04-25 20:43:09 +0200

    Add identifiers test for getAccessorChain

--- src/tests/getAccessorChain.spec.ts
+++ src/tests/getAccessorChain.spec.ts
@@ -14,4 +14,13 @@ it('transforms accessor function in array of consecutive accessors', () => {
   expect(getAccessorChain(_ => _)).toEqual([])
   expect(getAccessorChain(_ => _.b)).toEqual(['b'])
   expect(getAccessorChain(_ => _.b.c)).toEqual(['b', 'c'])
+  expect(getAccessorChain(_ => _['b']['c'])).toEqual(['b', 'c'])
+})
+
+it('accepts identifiers', () => {
+  const a = '13'
+  const b = '42'
+
+  expect(getAccessorChain(_ => _[a])).toEqual([a])
+  expect(getAccessorChain(_ => _[a][b])).toEqual([a, b])
 })
