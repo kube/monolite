ab6b1d53b3aadd20b3ee652faccbbc484e2f8123
Chris Feijoo <kube@users.noreply.github.com>
2018-03-08 14:43:42 +0100

    Prettier (#24)
    * Add Prettier and Format Code
    
    * Add VSCode formatOnSave workspace configuration
    
    * Add Prettier + Header Formatting on Precommit hook

--- src/deepEqual.ts
+++ src/deepEqual.ts
@@ -9,12 +9,12 @@
       ## ## ##*/
 
 const arrayEqual = (a: any[], b: any[]) => {
-  if (a.length !== b.length)
-    return false
+  if (a.length !== b.length) return false
 
   for (const index in a)
-    if (!deepEqual(a[index], b[index]))
+    if (!deepEqual(a[index], b[index])) {
       return false
+    }
 
   return true
 }
@@ -23,33 +23,37 @@ const objectEqual = (a: any, b: any) => {
   const propsA = Object.getOwnPropertyNames(a)
   const propsB = Object.getOwnPropertyNames(b)
 
-  if (propsA.length !== propsB.length)
+  if (propsA.length !== propsB.length) {
     return false
+  }
 
   for (const prop of propsA)
-    if (!Object.prototype.hasOwnProperty.call(b, prop)
-      || !deepEqual(a[prop], b[prop]))
+    if (
+      !Object.prototype.hasOwnProperty.call(b, prop) ||
+      !deepEqual(a[prop], b[prop])
+    ) {
       return false
+    }
 
   return true
 }
 
 const deepEqual = (a: any, b: any) => {
-  if (a === b)
+  if (a === b) {
     return true
-
-  if (typeof a !== typeof b)
+  }
+  if (typeof a !== typeof b) {
     return false
-
-  if (a === null || b === null)
+  }
+  if (a === null || b === null) {
     return false
-
-  if (Array.isArray(a))
+  }
+  if (Array.isArray(a)) {
     return arrayEqual(a, b)
-
-  if (typeof a === 'object')
+  }
+  if (typeof a === 'object') {
     return objectEqual(a, b)
-
+  }
   return a.valueOf() === b.valueOf()
 }
 
