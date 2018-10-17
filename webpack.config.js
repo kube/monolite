2803cb7d46acaebee688f0093a56818f0b4dfe2b
Chris Feijoo <christophe@feijoo.fr>
2018-10-17 14:56:53 +0200

    Bundle using webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+      /*#######.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##*/
+
+const { join } = require('path')
+
+const PROJECT_ROOT = __dirname
+const SOURCES_ROOT = join(PROJECT_ROOT, 'src')
+const BUILD_FOLDER = join(PROJECT_ROOT, 'lib')
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  mode: 'production',
+  context: SOURCES_ROOT,
+  entry: './index.ts',
+  output: {
+    path: BUILD_FOLDER,
+    filename: 'index.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
